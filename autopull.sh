6e5e1e7a06d78c830e9f90a6c000e0ea7f925d11
gorkemsolun <gorkemkadir14@hotmail.com>
2025-04-21 00:53:58 +0300

    Refactor autopull.sh to use placeholders for username and password

--- autopull.sh
+++ autopull.sh
@@ -2,7 +2,7 @@
 # execute git pull and then enter username, personel access token using 'expect'
 spawn ./gitpull.sh
 expect -exact "Username for 'https://github.com': "
-send -- "gorkemsolun\r"
-expect -exact "Password for 'https://gorkemsolun@github.com':"
-send -- "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\r"
+send -- "<username>\r"
+expect -exact "Password for 'https://<username>@github.com':"
+send -- "<password>\r"
 expect eof
